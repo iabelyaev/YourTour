3300842246edd0040692b678c1c11a7e8ab1fad9
Yaroslav <yibelyaev@icloud.com>
2023-08-08 22:03:23 +0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -38,7 +38,7 @@ const html = () => {
 // Scripts
 
 const scripts = () => {
-  return gulp.src('src/js/scripts.js')
+  return gulp.src('src/js/*.js')
   .pipe(terser())
   .pipe(gulp.dest('build/js'))
 }
@@ -128,7 +128,7 @@ const reload = (done) => {
 
 const watcher = () => {
   gulp.watch('src/sass/**/*.scss', gulp.series(styles));
-  gulp.watch('src/js/script.js', gulp.series(scripts));
+  gulp.watch('src/js/*.js', gulp.series(scripts, reload));
   gulp.watch('src/*.html', gulp.series(html, reload))
   gulp.watch('src/img/icons/*.svg').on('change', svg, browser.reload);
 }
---
+++ src/js/scripts.js
@@ -0,0 +1,11 @@
+// const navBar = document.querySelector('nav');
+
+
+// window.onscroll = function () {
+//   if(window.scrollY > 450) {
+//     navBar.classList.add('menu--scroll')
+//   } else {
+//     navBar.classList.remove('menu--scroll')
+//   }
+
+// }
