f7550334cd41171dbe76532923f7081ebf1c0af5
Yaroslav <yibelyaev@icloud.com>
2023-10-20 17:32:54 +0300

    Commented out the code and deleted console.log

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,12 +1,12 @@
 const navBar = document.querySelector('.navigation');
-console.log(navBar)
 
-// function scroll () {
-//   if (window.scrollY > 450) {
-//     navBar.classList.add('navigation--scroll')
-//   } else {
-//     navBar.classList.remove('navigation--scroll')
-//   }
-// }
+
+const scroll = () => {
+  if (window.scrollY > 450) {
+    navBar.classList.add('navigation--scroll')
+  } else {
+    navBar.classList.remove('navigation--scroll')
+  }
+}
 
 window.addEventListener('scroll', scroll)
